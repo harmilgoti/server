b5bd009b298b8a4d8374454e3cfb815e2ababedd
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 04:16:47 +0530

    fix: Refactor getUsers return type for consistency and update formatting

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,12 @@
-import { User } from './types';
+import { User } from "./types";
 
 // In-memory store
 const users: User[] = [];
 
-export const getUsers = (): User[] => {
-    return users;
+export const getUsers = (): { users: User[] } => {
+  return { users: users };
 };
 
 export const addUser = (user: User): void => {
-    users.push(user);
+  users.push(user);
 };
