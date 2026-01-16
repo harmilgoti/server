18121d36ab47f62e2f6dee5c13d62bec2a670eee
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 04:24:43 +0530

    fix: Refactor getUsers to return structured data with total count

--- src/store.ts
+++ src/store.ts
@@ -3,8 +3,11 @@ import { User } from "./types";
 // In-memory store
 const users: User[] = [];
 
-export const getUsers = (): User[] => {
-  return users;
+export const getUsers = (): { data: User[]; total: number } => {
+  return {
+    data: users,
+    total: users.length,
+  };
 };
 
 export const addUser = (user: User): void => {
