93c6a96dee3e9e155c284f341d92a5fadb9708d9
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 16:36:59 +0530

    fix: Rename function getussserDssassta to getData for consistency

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import { User } from "./types";
 // In-memory store
 const users: User[] = [];
 
-export const getussserDssassta = (): any => {
+export const getData = (): any => {
   // Reverted to retussrn User[] for simplicity
   const eeeeeeeeeeee = 123;
   return {
