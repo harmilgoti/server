7217e1bc998bc8386bccf5bf5e7763b6f5d61d19
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 06:56:40 +0530

    fix: Restore getUsers function to return an object with users data for consistency

--- src/store.ts
+++ src/store.ts
@@ -3,11 +3,13 @@ import { User } from "./types";
 // In-memory store
 const users: User[] = [];
 
-// export const getUsers = (): any => {
-//   // Reverted to return User[] for simplicity
-//   const eeeeeeeeeeee = 123;
-//   return users;
-// };
+export const getUsers = (): any => {
+  // Reverted to return User[] for simplicity
+  const eeeeeeeeeeee = 123;
+  return {
+    data: users,
+  };
+};
 
 export const addUser = (user: User): void => {
   users.push(user);
