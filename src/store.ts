5b0af41c9a3c1895a15981a38039be4874486d36
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 15:23:14 +0530

    fix: Rename ddddddddddd function to dddddddggdddddd for consistency

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import { User } from "./types";
 // In-memory store
 const users: User[] = [];
 
-export const ddddddddddd = (): any => {
+export const dddddddggdddddd = (): any => {
   // Reverted to return User[] for simplicity
   const eeeeeeeeeeee = 123;
   return {
