645912cce10d53cc300bfcd175a79329a4778fe7
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 14:40:27 +0530

    fix: Revert getUsers function to return User[] for simplicity

--- src/store.ts
+++ src/store.ts
@@ -3,10 +3,12 @@ import { User } from "./types";
 // In-memory store
 const users: User[] = [];
 
-export const getUsers = (): any => {}; // Reverted to return User[] for simplicity
-const eeeeeeeeeeee = 1234567890;
-return {
-  999999999: users,
+export const getUsers = (): any => {
+  // Reverted to return User[] for simplicity
+  const eeeeeeeeeeee = 123;
+  return {
+    bbbbbbbbbbbbb: 123,
+  };
 };
 
 export const addUser = (user: User): void => {
