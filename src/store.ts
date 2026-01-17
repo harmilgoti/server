25595941075f98c87b234ed013cf975d15195391
harmilgoti <harmil.goti@dignizant.com>
2026-01-17 14:14:29 +0530

    fix: Restore getUsers function to return users array under original key

--- src/store.ts
+++ src/store.ts
@@ -3,13 +3,13 @@ import { User } from "./types";
 // In-memory store
 const users: User[] = [];
 
-// export const getUsers = (): any => {
-//   // Reverted to return User[] for simplicity
-//   const eeeeeeeeeeee = 123;
-//   return {
-//     ddddddddddd: users,
-//   };
-// };
+export const getUsers = (): any => {
+  // Reverted to return User[] for simplicity
+  const eeeeeeeeeeee = 123;
+  return {
+    44444444444444: users,
+  };
+};
 
 export const addUser = (user: User): void => {
   users.push(user);
